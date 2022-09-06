d953684ddfb51631853e101dce8db0ea35d86cfd
AnselmoCristian <85968166+AnselmoCristian@users.noreply.github.com>
2022-09-06 13:42:17 -0300

    Add files via upload

--- La2 Virtual Campus - FrontEnd/src/app/modules/dashboard/views/contact_us/contact_us.component.ts
+++ La2 Virtual Campus - FrontEnd/src/app/modules/dashboard/views/contact_us/contact_us.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fte-contact-us',
   templateUrl: './contact_us.component.html',
   styleUrls: ['./contact_us.component.scss']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent {
+
+  // Inputs
+  formContact = new FormGroup({
+    from: new FormControl('', { validators: [ Validators.required ] }),
+    to: new FormControl('', { validators: [ Validators.required ] }),
+    subject: new FormControl('', { validators: [ Validators.required ] }),
+    password: new FormControl('', { validators: [ Validators.required ] }),
+  });
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
